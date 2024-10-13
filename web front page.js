2ee28c78e5deafeec0cba733718389b02b4c9814
Ankur1079 <ankuryadav1079@gmail.com>
2024-10-13 11:58:45 +0530

    Create web front page.js

---
+++ web front page.js
@@ -0,0 +1,11 @@
+document.getElementById('homeLink').addEventListener('click', function() {
+    alert('Welcome to Home Page!');
+});
+
+document.getElementById('searchLink').addEventListener('click', function() {
+    alert('Search functionality coming soon!');
+});
+
+document.getElementById('helpLink').addEventListener('click', function() {
+    alert('How can we assist you?');
+});
